d6c6fcf024bc9c614f3bab593005581a69e07ce0
PhanVanDiej <22520182@gm.uit.edu.vn>
2024-11-04 16:13:50 +0700

    add

--- ThucHanhWebMVC/obj/Debug/net8.0/ThucHanhWebMVC.AssemblyInfo.cs
+++ ThucHanhWebMVC/obj/Debug/net8.0/ThucHanhWebMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThucHanhWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a55997e818c50ea02eb986e6508f7e5e47991f72")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThucHanhWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThucHanhWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
